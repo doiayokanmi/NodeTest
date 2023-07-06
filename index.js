3c5ac8eaebcecbc02d086b8c1c2c1061fe44b974
Ibrahim Damilola Dhikirullahi <oloyedammy@gmail.com>
2023-07-06 07:31:39 -0700

    Saving to Database

--- index.js
+++ index.js
@@ -1,24 +1,34 @@
-const express = require('express')
-const app = express()
-const dotenv = require('dotenv')
-dotenv.config()
-app.set('view engine', 'ejs')
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+const bodyparser = require("body-parser");
+app.use(bodyparser.urlencoded({ extended: true }));
+const { default: mongoose, model } = require("mongoose");
 
+dotenv.config();
+app.set("view engine", "ejs");
 
-require("./models/mongoose.connect")
+require("./models/mongoose.connect");
 
-let port = process.env.port
+let port = process.env.port;
 
+const itemSchema = new mongoose.Schema({
+  item: { type: String, required: true },
+  amount: { type: Number, required: true },
+});
+
+const itemModel = mongoose.model("Items", itemSchema);
 
 app.get("/", (req, res) => {
-    res.render("index")
-})
+  res.render("index");
+});
 
-app.post("/details", (req, res)=> {
+app.post("/details", (req, res) => {
+  console.log(req.body);
+  form = new itemModel(req.body);
+  form.save();
+});
 
-})
-
-
-app.listen(port, ()=> {
-    console.log("Server Started");
-})
+app.listen(port, () => {
+  console.log("Server Started");
+});
